d950e4db583ff789cbf0e580dd1a11ba91367152
Danilo0528 <dpino131@gmail.com>
2025-06-05 00:41:25 -0500

    Credenciales

---
+++ web/my-restaurant/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCSNqssPe7-Zf3n7b9vBRIVluDnadSe0kI",
+  authDomain: "restauraniud.firebaseapp.com",
+  projectId: "restauraniud",
+  storageBucket: "restauraniud.firebasestorage.app",
+  messagingSenderId: "362578909875",
+  appId: "1:362578909875:web:b7519a01ddd8f032d5eec6",
+  measurementId: "G-VE0NX3JE8S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
